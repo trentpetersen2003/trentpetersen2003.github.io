a15c71246ca3dcb9d317ef3fc032e24f563ad557
Trent Petersen <trentep@email.sc.edu>
2024-10-07 23:49:51 -0400

    project part 5

---
+++ csce242/projects/part5/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    toggleButton.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+});
